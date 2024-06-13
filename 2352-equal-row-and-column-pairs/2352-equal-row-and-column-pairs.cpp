4af01ae551efe0801a0d37ca2fa3a9acbeb12e92
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-06-13 14:18:20 +0530

    Time: 112 ms (38.04%), Space: 46.5 MB (29.44%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
- 
-
     int equalPairs(vector<vector<int>>& grid) {
         unordered_map<string, int> rowMap;
-        unordered_map<string, int> colMap;
         int n = grid.size();
 
+        // Count row occurrences
         for (int i = 0; i < n; i++) {
             string num = "";
             for (int j = 0; j < n; j++) {
-                num += to_string( grid[i][j]);
+                num += to_string(grid[i][j]) + ",";
             }
             rowMap[num]++;
         }
 
-   
-        for (int i = 0; i < n; i++) {
-             string num1 = "";
-            for (int j = 0; j < n; j++) {
-                num1 += to_string( grid[j][i]);
-            }
-          
-            colMap[num1]++;
-        }
 
-     
         int comb = 0;
-        for (const auto& [key, value] : rowMap) {
-            if (colMap.find(key) != colMap.end()) {
-                comb += value * colMap[key];
+        for (int i = 0; i < n; i++) {
+            string num = "";
+            for (int j = 0; j < n; j++) {
+                num += to_string(grid[j][i]) + ",";
+            }
+            if (rowMap.find(num) != rowMap.end()) {
+                comb += rowMap[num];
             }
         }
 
