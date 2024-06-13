fa5dcbd88ae753b815833af41781ca7297b93df4
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-06-13 15:34:22 +0530

    Time: 7 ms (85.04%), Space: 20.4 MB (69.8%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,66 +1,44 @@
 #include <vector>
 #include <stack>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> stack;
-        if (asteroids.size() <= 1)
-            return asteroids;
-        stack.push(asteroids[0]);
-        int maxim=0;
-        for (int i = 1; i < asteroids.size(); i++) {
-            if (!stack.empty()) {
-                if ((stack.top() > 0 && asteroids[i] < 0) ) {
-                    int val = stack.top();
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            int curr = asteroids[i];
+
+
+            while (!stack.empty() && stack.top() > 0 && curr < 0) {
+                int top = stack.top();
+                if (abs(top) < abs(curr)) {
                     stack.pop();
-                    if (val + asteroids[i] != 0){
-                       maxim=max(abs(val),abs(asteroids[i]));
-                       if(maxim+val==0||maxim+val==2*val )
-                       stack.push(val);
-                       if(maxim+asteroids[i]==0||maxim+asteroids[i]==2*asteroids[i] )
-                       stack.push(asteroids[i]);
-                    }
+                } else if (abs(top) == abs(curr)) {
+                    stack.pop(); 
+                    curr = 0;   
+                    break;      
                 } else {
-                    stack.push(asteroids[i]);
-                }           
-                
-                while (stack.size() > 1 && ((stack.top() < 0 && secondElement(stack) > 0) )) {
-                    int val = stack.top();
-                    stack.pop();
-                    int val2 = stack.top();
-                    stack.pop();
-                    if (val + val2 != 0)
-                       {
-                       maxim=max(abs(val),abs(val2));
-                        if(maxim+val2==0||maxim+val2==2*val2 )
-                       stack.push(val2);
-                       if(maxim+val==0||maxim+val==2*val )
-                       stack.push(val);
-                      
-                    }
-                } 
-                
-            } else {
-                stack.push(asteroids[i]);
-            }            
+                    curr = 0;    
+                    break;       
+                }
+            }
+            
+          
+            if (curr != 0) {
+                stack.push(curr);
+            }
         }
-        
-        vector<int> v(stack.size());
+
+    
+        vector<int> result(stack.size());
         for (int i = stack.size() - 1; i >= 0; i--) {
-            v[i] = stack.top();
+            result[i] = stack.top();
             stack.pop();
         }
-        return v;
-    }
-    
-private:
-    int secondElement(stack<int>& s) {
-        int top = s.top();
-        s.pop();
-        int second = s.top();
-        s.push(top);
-        return second;
+
+        return result;
     }
 };
