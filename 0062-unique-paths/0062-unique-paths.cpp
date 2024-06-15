c455fb5bcb8e764d42ad5cebed53f03c4a624057
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-06-15 19:22:13 +0530

    Time: 0 ms (100%), Space: 7.6 MB (63%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int recur(int m,int n,vector<vector<int>>&dp){
+        if(m==0 && n==0)
+        {
+            
+            return 1;
+        }
+        if(m<0 || n<0)
+        return 0;
+        if(dp[m][n]!=-1)
+        return dp[m][n];
+        int left=recur(m-1,n,dp);
+        int top=recur(m,n-1,dp);
+        return dp[m][n]=left+top;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return recur(m-1,n-1,dp);
+        
+    }
+};
