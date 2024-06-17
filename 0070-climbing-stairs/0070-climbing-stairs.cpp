9e56d82a4e91d0e1e519fe5ef83c87c0e8c9cf11
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-06-17 12:28:20 +0530

    Time: 0 ms (100%), Space: 7.2 MB (50.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int recur(int n,vector<int>&dp)
     {
-        if(n<=1)
-        return 1;
+        if(n<=2 &&n!=0)
+        return n;
         if(dp[n]!=-1)
         return dp[n];
         int one=recur(n-1,dp);
