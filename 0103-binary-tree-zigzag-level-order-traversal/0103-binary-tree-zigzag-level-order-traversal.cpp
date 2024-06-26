a9e6d3615db4a6563171392a570ecd044352a5e6
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-06-26 21:58:18 +0530

    Time: 7 ms (11.07%), Space: 13 MB (88.91%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,47 +1,41 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
  */
-#include <vector>
-#include <algorithm>
-using namespace std;
 
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
-        }
-        int left = height(root->left);
-        int right = height(root->right);
-        return max(left, right) + 1;
-    }
-
-    void traversal(int level, TreeNode* root, vector<vector<int>>& out, bool leftToRight) {
-        if (root == nullptr) return;
-        
-        out[level].push_back(root->val);
-        
-        if (leftToRight) {
-            traversal(level + 1, root->left, out, !leftToRight);
-            traversal(level + 1, root->right, out, !leftToRight);
-        } else {
-            traversal(level + 1, root->right, out, !leftToRight);
-            traversal(level + 1, root->left, out, !leftToRight);
-        }
-    }
-
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        int h = height(root);
-        vector<vector<int>> out(h);
-        traversal(0, root, out, false);  
+        if (!root) return {}; 
+        
+        queue<TreeNode*> q;
+        vector<vector<int>> out;
+        q.push(root);
+        bool check = true;
+        
+        while (!q.empty()) {
+            int l = q.size();
+            vector<int> v(l);
+            
+            for (int i = 0; i < l; i++) {
+                TreeNode* ptr = q.front();
+                q.pop();
+                int ind = check ? i : l - i - 1;
+                v[ind] = ptr->val;
+                
+                if (ptr->left)
+                    q.push(ptr->left);
+                if (ptr->right)
+                    q.push(ptr->right);
+            }
+            
+            out.push_back(v); 
+            check = !check; 
+        }
+        
         return out;
     }
 };
