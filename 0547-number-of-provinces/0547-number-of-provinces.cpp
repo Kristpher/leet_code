6acc768c51d3fff86759e6630c0d9bb9d1e39c67
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-09 10:40:50 +0530

    Time: 22 ms (50.29%), Space: 18.1 MB (74.78%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void province(vector<int>& v, vector<vector<int>>& isConnected, int ind) {
+       
+        v[ind] = 1;
+
+    
+        for (int j = 0; j < isConnected.size(); j++) {
+            if (isConnected[ind][j] == 1 && v[j] == 0) {
+                province(v, isConnected, j);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<int> v(isConnected.size(), 0);
+        int count = 0;
+
+     
+        for (int i = 0; i < isConnected.size(); i++) {
+          
+            if (v[i] == 0) {
+                count++;
+                province(v, isConnected, i);
+            }
+        }
+
+        return count;
+    }
+};
