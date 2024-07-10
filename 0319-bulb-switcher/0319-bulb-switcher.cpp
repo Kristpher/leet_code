d01d8260ef784c0cd2c49476b4bbe750b77d2d46
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-10 10:39:42 +0530

    Time: 2 ms (57.61%), Space: 7.2 MB (24.13%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
-            int k=sqrt(i);
-            if(k*k==i)
-            count++;
-        }
-        return count;
+    
+        return sqrt(n);
     }
 };
