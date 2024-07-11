1beb7029edf6e97b4e280dff6310a0555693316f
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-11 11:10:43 +0530

    Time: 2 ms (53.02%), Space: 11.3 MB (7.3%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -6,44 +6,24 @@ public:
         string temp = "";
         for (int i = 0; i < s.length(); i++) {
             if (s[i] != ')') {
-                if (s[i] == '(')
-                    count++;
                 stack.push(s[i]);
             } else {
-                if(count%2==0){
-                while (stack.top() != '(' ) {
-                    //cout<<stack.top();
+                while (stack.top() != '(') {
                     temp = stack.top() + temp;
                     stack.pop();
                 }
-                if ( stack.top() == '(') {
+                if (stack.top() == '(') {
                     stack.pop();
-                    count--;
-                }
-                }
-                else{
-                while (stack.top() != '(' ) {
-                     //cout<<stack.top();
-                    temp= stack.top()+temp;
-                    
-                    stack.pop();
-                }
-                if (  stack.top() == '(') {
-                    stack.pop();
-                    count--;
-                }
                 }
                 while (!temp.empty()) {
                     stack.push(temp[temp.length() - 1]);
-                    cout<<stack.top();
                     temp.pop_back();
                 }
-                cout<<endl;
             }
         }
         string out = "";
         while (!stack.empty()) {
-            out = stack.top()+out;
+            out = stack.top() + out;
             stack.pop();
         }
         return out;
