350df3e7904cc30145eb23c277d30e8d0259f0c2
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-19 13:47:40 +0530

    Time: 40 ms (39.56%), Space: 23 MB (53.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,42 +1,52 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> wordCount;
         vector<int> result;
+        if (s.empty() || words.empty()) return result;
+
         int wordLength = words[0].length();
         int totalWords = words.size();
         int totalLength = wordLength * totalWords;
-        
+
         if (s.length() < totalLength) return result;
 
-
+        unordered_map<string, int> wordCount;
         for (const string& word : words) {
             wordCount[word]++;
         }
 
-        for (int i = 0; i <= s.length() - totalLength; i++) {
+        for (int i = 0; i < wordLength; i++) {
             unordered_map<string, int> seenWords;
-            int j = 0;
-           
-            while (j < totalWords) {
-                string word = s.substr(i + j * wordLength, wordLength);
+            int left = i;
+            int count = 0;
+            
+            for (int right = i; right <= s.length() - wordLength; right += wordLength) {
+                string word = s.substr(right, wordLength);
+                
                 if (wordCount.count(word)) {
                     seenWords[word]++;
-                 
-                    if (seenWords[word] > wordCount[word]) {
-                        break;
+                    count++;
+                    
+                    
+                    while (seenWords[word] > wordCount[word]) {
+                        string leftWord = s.substr(left, wordLength);
+                        seenWords[leftWord]--;
+                        count--;
+                        left += wordLength;
+                    }
+                    
+                    
+                    if (count == totalWords) {
+                        result.push_back(left);
                     }
                 } else {
-                    break;
+                    seenWords.clear();
+                    count = 0;
+                    left = right + wordLength;
                 }
-                j++;
-            }
-          
-            if (j == totalWords) {
-                result.push_back(i);
             }
         }
-
+        
         return result;
     }
 };
