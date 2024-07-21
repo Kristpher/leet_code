e8a359532370bf19f00605d824a87188c95e6465
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-21 21:26:15 +0530

    Time: 314 ms (5%), Space: 20.8 MB (5.95%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
- int recur(vector<int>& nums,vector<int>& dp,int n)
- {
-    
-    if(n>=nums.size()-1)
-    {
-        return 1;
-    }
-     if(nums[n]==0)
-    return 0;
-   
+    int recur(vector<int>& nums, vector<int>& dp, int n) {
+        if (n >= nums.size() - 1) {
+            return 0; 
+        }
+        if (nums[n] == 0) {
+            return INT_MAX;
+        }
 
-    if(dp[n]!=-1)
-    return dp[n];
-    int maxim=0;
-    for(int i=1;i<=nums[n];i++){
-    int left=recur(nums,dp,n-i)+1;
-    maxim=max(left,maxim);
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        int minJumps = INT_MAX;
+        for (int i = 1; i <= nums[n]; ++i) {
+            int result = recur(nums, dp, n + i);
+            if (result != INT_MAX) { 
+                minJumps = min(minJumps, result + 1);
+            }
+        }
+
+        dp[n] = minJumps;
+        return dp[n];
     }
-    return dp[n]=maxim;
- }
+
     int jump(vector<int>& nums) {
-   if(nums.size()==1)
-   return 0;
-   vector<int>dp(nums.size(),-1);
-
-    return recur(nums,dp,0);     
+        if (nums.size() == 1) {
+            return 0;
+        }
+        vector<int> dp(nums.size(), -1);
+        return recur(nums, dp, 0);
     }
 };
