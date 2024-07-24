387ec6e6bfafeae7093be48248ff0fd2728737e5
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-24 16:58:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int recur(vector<int>& nums, int n, vector<int>& dp) {
-        if (n < 0) {
-            return 0;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        int take = nums[n] + recur(nums, n - 2, dp);
-        int not_take = recur(nums, n - 1, dp);
-        return dp[n] = max(take, not_take);
+    int recur(vector<int>& nums,vector<int>& dp,int n){
+        if(n<0)
+        return 0;
+        int two=recur(nums,dp,n-2)+nums[n];
+        int one=recur(nums,dp,n-1);
+        return dp[n]=max(one,two);
     }
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return nums[0];
-        }
-        vector<int> dp(n, -1);
-        return recur(nums, n - 1, dp);
+         if(nums.size()==0)
+         return 0;
+         if(nums.size()==1)
+         return nums[0];
+        vector<int>dp(nums.size(),-1);
+        return recur(nums,dp,nums.size()-1);
     }
 };
