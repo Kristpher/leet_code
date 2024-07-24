ec63d61274fb628a76bb518e4a8614c7c075f20c
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-07-24 12:09:48 +0530

    Time: 4 ms (73.24%), Space: 11.6 MB (22.49%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-  
-     int recur(vector<vector<int>>& triangle, int i,int j,int n,vector<vector<int>>& dp){
-	if(i==n-1)
-	{
-		return triangle[n-1][j];
-	}
-   if(dp[i][j]!=-1)
-   return dp[i][j];
-   int down=recur(triangle,i+1,j,n,dp)+triangle[i][j];
-   int diagonal=recur(triangle,i+1,j+1,n,dp)+triangle[i][j];
-    int minim=INT_MAX;
-   minim=min(down,diagonal);
-   return dp[i][j]=minim;
-}
+    int recur(vector<vector<int>>& triangle, vector<vector<int>>& dp, int n, int m) {
+        if (n < 0 || m < 0 || m >n)
+            return INT_MAX; 
 
- int minimumTotal(vector<vector<int>>& triangle){
-int n=triangle.size();
- vector<vector<int>>dp(n,vector<int>(n+1,-1));
- return recur(triangle,0,0,n,dp);
- 
-}
+        if (n == 0 && m == 0)
+            return triangle[0][0]; 
+
+        if (dp[n][m] != -1)
+            return dp[n][m];
+            
+        int up = recur(triangle, dp, n - 1, m);
+        if (up != INT_MAX) {
+            up += triangle[n][m];
+        }
+        
+        int down = recur(triangle, dp, n - 1, m - 1);
+        if (down != INT_MAX) {
+            down += triangle[n][m];
+        }
+        
+        return dp[n][m] = min(up, down);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (n == 0) return 0;
+
+        vector<vector<int>> dp(n);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = vector<int>(triangle[i].size(), -1);
+        }
+
+        int minim = INT_MAX;
+        for (int i = 0; i < triangle[n-1].size(); ++i) {
+            minim = min(minim, recur(triangle, dp, n - 1, i));
+        }
+
+        return minim;
+    }
 };
