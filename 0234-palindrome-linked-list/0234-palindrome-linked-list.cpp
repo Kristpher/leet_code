c456bed71746fdbc573a25e332e4193fb4c1e1b0
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-08-03 12:19:20 +0530

    Time: 128 ms (98.19%), Space: 116.8 MB (75.72%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return true;
+        }
+
+        ListNode* ptr = head;
+        ListNode* ptr2 = head;
+        ListNode* prev = nullptr;
+
+        while (ptr2 != NULL && ptr2->next != NULL) {
+            ptr2 = ptr2->next->next;
+            prev = ptr;
+            ptr = ptr->next;
+        }
+
+        if (ptr2 != NULL) {
+            ptr = ptr->next;
+        }
+
+        prev->next = nullptr;
+        ptr2 = nullptr;
+
+        while (ptr != NULL) {
+            ListNode* next = ptr->next;
+            ptr->next = ptr2;
+            ptr2 = ptr;
+            ptr = next;
+        }
+
+        ptr = head;
+
+        while (ptr2 != NULL) {
+            if (ptr == NULL || ptr->val != ptr2->val) {
+                return false;
+            }
+            ptr = ptr->next;
+            ptr2 = ptr2->next;
+        }
+
+        return true;
+    }
+};
