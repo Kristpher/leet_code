6fe948007a2907ae2b5b250d02e06e95ffa6c202
Navaneeth D S <163757837+Kristpher@users.noreply.github.com>
2024-08-06 21:31:37 +0530

    Time: 447 ms (5.1%), Space: 26.1 MB (50.17%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    static bool comp(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+
+    int minimumPushes(string word) {
+        vector<pair<int, int>> v(26, make_pair(0, 0));
+        for (int i = 0; i < 26; i++) {
+            v[i].first = i;
+            v[i].second = 0;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            v[word[i] - 'a'].second++;
+             cout<<v[word[i] - 'a'].second;
+        }
+        sort(v.begin(), v.end(), comp);
+        int count = 0;
+        int press = 0;
+        int val = 1;
+        for (int i = 0; i < 26; i++) {
+            if (v[i].second == 0)
+                break;
+            press += val*v[i].second;
+            count++;
+            if (count == 8) {
+                val++;
+                count = 0;
+            }
+        }
+        return press;
+    }
+};
